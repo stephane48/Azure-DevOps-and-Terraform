fa12e19f32c13edcfd638c335e1f428cb9db1c8d
stephane kamdem kamguia <skamdem-kamguia@myseneca.ca>
2022-07-04 15:04:57 -0400

    Fixedour pipeline

--- main.tf
+++ main.tf
@@ -4,6 +4,23 @@ provider "azurerm" {
   features {}
 }
 
+# Create a backend to store the tfstate file, we store it in a container
+# created under a storage account on azure
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rd_blobstore"
+        storage_account_name = "tfstorageaccountstephane"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
+# Create the variable for the tag of our docker image
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
+}
+
 # Create resource group in Azure
 resource "azurerm_resource_group" "tf_test" {
   name = "tfminrg"
@@ -21,10 +38,10 @@ resource "azurerm_container_group" "tfcg_test" {
   os_type           = "Linux" 
 
   container {
-    name = "weatherapi"
-    image   = "stephane48/weatherapi" 
-        cpu     = "1"
-        memory = "1"
+    name            = "weatherapi"
+    image           = "stephane48/weatherapi:${var.imagebuild}" 
+        cpu         = "1"
+        memory      = "1"
 
         ports {
             port     = 80
